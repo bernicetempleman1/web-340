3cdbe0177451edce2390df00922ae286e9bf9da6
Bernice Templeman <bernice.templeman1@gmail.com>
2024-03-31 10:22:25 -0400

    initial commit week-2

--- week-2/cooking-app/index.js
+++ week-2/cooking-app/index.js
@@ -1,10 +1,22 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Bernice Templeman
+ * Date: 03/31/2024
+ * File Name: index.js
+ * Description: Use the index.js file to demonstrate the functionality of your recipe modules
 */
 
 // TODO: Import your module using require
+// Try to import the recipe module
+
+const { createRecipe, setTimer, quit } = require("./recipes.js");
+
 
 // TODO: Implement your CLI program here
+function main() {
+  const ingredients = ["Greens", "Beans", "Crushed tomatoes", "garlic", "onion", "parsley", "potatoes"];
+  console.log(createRecipe(ingredients)); // call the create recipe function
+  console.log(setTimer(15)); //call the setTimer function
+  console.log(quit()); // call the quit function
+  }
+
+  main(); // call the main() function
--- week-2/cooking-app/recipes.js
+++ week-2/cooking-app/recipes.js
@@ -1,23 +1,42 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
-*/
+ * Author: Bernice Templeman
+ * Date: 03/31/2024
+ * File Name: recipe.js
+ * Description: Recipe Modules: Contains functions: createRecipe, setTimer, and quit
+ */
 
-// Define the createRecipe function
+// Define the createRecipe function This function should take an array of ingredients and return:
+// “Recipe created with ingredients: ingredient1, ingredient2”
 function createRecipe(ingredients) {
   // TODO: Implement this function
+  let ingredientString = 'Recipe created with ingredients:';
+  // loop over the ingredient array and output the results to a string
+  for (let i = 0; i < ingredients.length; i++) {
+    ingredientString +=  ' ' + ingredients[i];
+    if (i < ingredients.length-1){
+      ingredientString += ',';
+    }
+  }
+  return ingredientString;
 }
 
 // Define the setTimer function
+// This function should take a number of minutes and return a string in the format: “Timer set for X minutes."
 function setTimer(minutes) {
   // TODO: Implement this function
+  return "Timer set for " + minutes + " minutes"
 }
 
 // Define the quit function
+// This function should return the string “Program exited"
 function quit() {
   // TODO: Implement this function
+  return "Program exited";
 }
 
 // TODO: Export the functions
+  module.exports = {
+  createRecipe: createRecipe,
+  setTimer: setTimer,
+  quit: quit
+};
