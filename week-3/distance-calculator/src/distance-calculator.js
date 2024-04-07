d1db86d8b4453ea8a95ab68a1f4686bf24616eb2
Bernice Templeman <bernice.templeman1@gmail.com>
2024-04-07 19:00:51 -0400

    initial commit week-3

--- week-3/distance-calculator/src/distance-calculator.js
+++ week-3/distance-calculator/src/distance-calculator.js
@@ -1,6 +1,45 @@
-"use strict";
+/**
+ * Author: Bernice Templeman
+ * Date: 04/06/2024
+ * File Name: distance-calculator.js
+ * Description: calculate the distance between two planets
+ */
+// function that calculates the distance between two planets in AU, given their distances from the sun in AU
 function calculateDistance(planet1, planet2) {
-  // TODO: Implement this function
-}
+  'use strict';
+  let planet1up = planet1.toUpperCase();
+  let planet2up = planet2.toUpperCase();
 
+  // planet distances from https://www.jpl.nasa.gov/edu/pdfs/scaless_reference.pdf
+  const planetDistances = {
+    "MERCURY": 0.39,
+    "VENUS": 0.72,
+    "EARTH": 1,
+    "MARS": 1.52,
+    "JUPITER": 5.2,
+    "SATURN": 9.54,
+    "URANUS": 19.2,
+    "NEPTUNE": 30.06
+  };
+
+
+  // check if planet names are valid
+  if ((planet1up in planetDistances) === false) {
+    throw new RangeError ("Planet name must be a valid planet name");
+  } else if ((planet2up in planetDistances) === false) {
+    throw new  RangeError ('Planet name must be a valid planet name');
+  } else {
+    // get the planet distances
+    let planet1Distance = planetDistances[planet1up];
+    let planet2Distance = planetDistances[planet2up];
+
+    // calculate the distance between the two planets
+    let distance = planet1Distance - planet2Distance;
+    if (distance < 0) {
+      distance *= -1;
+    }
+    return(distance.toFixed(2) + ' AU');
+  }
+}
 module.exports = calculateDistance;
+
--- week-3/distance-calculator/test/distance-calculator.spec.js
+++ week-3/distance-calculator/test/distance-calculator.spec.js
@@ -1,9 +1,57 @@
-"use strict";
+/**
+ * Author: Bernice Templeman
+ * Date: 04/06/2024
+ * File Name: distance-calculator.spec.js
+ * Description: test distance-calculator.js
+ */
+'use strict';
 const assert = require('assert');
 const calculateDistance = require('../src/distance-calculator');
 
-function testFunctionDescription() {
-  // TODO: Implement this function
+// test the distance from Earth to Mars
+function testEarthToMars() {
+  try {
+    assert.strictEqual(calculateDistance('Earth', 'Mars'), '0.52 AU');
+    console.log('Passed testEarthToMars');
+    return true;
+  } catch (error) {
+    console.error(`Failed testEarthToMars: ${error.message}`);
+    return false;
+  }
+}
+
+// test the distance from Mars to Earth
+function testMarsToEarth() {
+  try {
+    assert.strictEqual(calculateDistance('Mars', 'Earth'), '0.52 AU');
+    console.log('Passed testMarsToEarth');
+    return true;
+  } catch (error) {
+    console.error(`Failed testMarsToEarth: ${error.message}`);
+    return false;
+  }
+}
+
+// test for valid planet names
+function testNotValidPlanetName() {
+  try {
+    assert.strictEqual(calculateDistance('Earth', 'Mar'), -1);
+    console.log('Passed testNotValidPlanetName');
+    throw new RangeError("Planet name must be a valid planet name'");
+  } catch (error) {
+    if (error.message === 'Planet name must be a valid planet name') {
+      console.log('Passed testNotValidPlanetName');
+      return true;
+    } else {
+      console.error(`Failed testNotValidPlanetName: ${error.message}`);
+      return false;
+    }
+  }
 }
 
 // Call your test functions here
+testEarthToMars();
+testMarsToEarth();
+testNotValidPlanetName();
+
+
