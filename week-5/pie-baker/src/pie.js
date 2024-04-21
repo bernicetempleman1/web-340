a3865576f64fb67d8f7611f7aff7a39efe47e4e6
Bernice Templeman <bernice.templeman1@gmail.com>
2024-04-21 15:28:47 -0400

    initial commit week-5

--- week-5/pie-baker/src/pie.js
+++ week-5/pie-baker/src/pie.js
@@ -1,13 +1,47 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Bernice Templeman
+ * Date: 04/21/2024
+ * File Name: pie.js
+ * Description: module that simulates a simple pie baker.
  */
-"use strict";
+'use strict';
 
 function bakePie(pieType, ingredients) {
   // Your code here
+  // The function should return a message indicating whether the pie was successfully baked or not.
+  // If an essential ingredient is missing, the function should log a warning message and call process.exit(1).
+  // The essential ingredients are: flour, sugar, and butter
+
+  const ingredientsLC = ingredients.map((ingredient) =>
+    ingredient.toLowerCase()
+  );
+
+  // check for missing ingredients
+  let missingFlag = false;
+  let message = 'Warning: Missing ingredients: ';
+  if (!ingredientsLC.includes('flour')) {
+    message += 'flour ';
+    missingFlag = true;
+  }
+  if (!ingredientsLC.includes('sugar')) {
+    message += 'sugar ';
+    missingFlag = true;
+  }
+  if (!ingredientsLC.includes('butter')) {
+    message += 'butter';
+    missingFlag = true;
+  }
+
+  // If an essential ingredient is missing, the function should log a warning message and call process.exit(1).
+  if (missingFlag) {
+    console.log(message);
+    process.exit(1);
+  } else if (pieType === 'PieName1') {
+    message = 'Success: Pie Successfully Baked!';
+  } else {
+    message = 'Failed Baking: All ingredients present!';
+  }
+  return message;
 }
 
 module.exports = { bakePie };
--- week-5/pie-baker/test/pie.spec.js
+++ week-5/pie-baker/test/pie.spec.js
@@ -1,12 +1,50 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Bernice Templeman
+ * Date: 04/21/2024
+ * File Name: pie.spec.js
+ * Description: unit tests for the bakePie function
  */
 
-"use strict";
+'use strict';
 
-const { bakePie } = require("../src/pie");
+const { bakePie } = require('../src/pie');
 
 // Your tests here
+
+const exit = jest.spyOn(process, 'exit').mockImplementation((code) => code);
+describe('bakePie', () => {
+  let log;
+  beforeEach(() => {
+    log = jest.spyOn(console, 'log');
+  });
+  afterEach(() => {
+    log.mockRestore();
+  });
+
+  // unit test 1 (condition that does not call process.exit)
+  test('Successfully Baked with all required ingredients', () => {
+    const ingredients = ['flour', 'sugar', 'butter'];
+    let pieType = 'PieName1';
+    let message = bakePie(pieType, ingredients);
+    expect(message).toBe('Success: Pie Successfully Baked!');
+    expect(exit).not.toHaveBeenCalled();
+  });
+
+  // unit test 2 (condition that does not call process.exit)
+  test('Failed Baking with all required ingredients', () => {
+    const ingredients = ['flour', 'sugar', 'butter'];
+    let pieType = 'PieName2';
+    let message = bakePie(pieType, ingredients);
+    expect(message).toBe('Failed Baking: All ingredients present!');
+    expect(exit).not.toHaveBeenCalled();
+  });
+
+  // unit test 3 (condition that does call process.exit)
+  test('identifies a missing ingredient', () => {
+    const ingredients = ['flour', 'sugar'];
+    let pieType = 'PieName3';
+    let message = bakePie(pieType, ingredients);
+    expect(log).toHaveBeenCalledWith('Warning: Missing ingredients: butter');
+    expect(exit).toHaveBeenCalledWith(1);
+  });
+});
