add374e605cc2f71b166c36af852883a8826d43e
Bernice Templeman <bernice.templeman1@gmail.com>
2024-05-05 19:24:45 -0400

    initial commit week-7

--- week-7/fantasy-character-creation-stream/src/character-creator.js
+++ week-7/fantasy-character-creation-stream/src/character-creator.js
@@ -1,17 +1,45 @@
+'use strict';
+/*
+  Author: Bernice Templeman
+  Date: 05/04/2024
+
+  Filename: character-creator.js
+
+  create a CharacterCreator class that extends the
+  Duplex stream class from Node.js’s built-in stream module.
+  The class should process character creation data and output a formatted description.
+
+  The CharacterCreator class should take data about the character’s class (Warrior,
+  Mage, Route), gender (Male, Female, Other), and a fun fact about the character.
+    • Use the push method in your Duplex stream to output data.
+    • Remember to call callback in your _write method.
+    • Use the done function in your Jest tests to handle asynchronous code.
+    • Refer to the MoviesStream provided in this chapter for guidance.
+
+*/
 const { Duplex } = require('stream');
 
 class CharacterCreator extends Duplex {
   constructor(options) {
     super(options);
-    // TODO: Initialize your class here
+    this.character = [];
   }
 
   _write(chunk, encoding, callback) {
-    // TODO: Implement your _write method here
+    const charData = chunk.toString();
+    if (!charData) {
+      callback(new Error('Invalid character data'));
+      return;
+    }
+    this.character= JSON.parse(charData);
+    callback();
   }
 
   _read(size) {
-    // TODO: Implement your _read method here
+    this.push(
+      `The character is a ${this.character.gender} class ${this.character.class} who is ${this.character.funFact}`
+    );
+    this.push(null);
   }
 }
 
--- week-7/fantasy-character-creation-stream/test/character-creator.spec.js
+++ week-7/fantasy-character-creation-stream/test/character-creator.spec.js
@@ -1,21 +1,55 @@
+'use strict';
+/*
+      Author: Bernice Templeman
+      Date: 05/04/2024
+
+      Filename: character-creator.spec.js
+*/
+
 const CharacterCreator = require('../src/character-creator');
 
 describe('CharacterCreator', () => {
   let characterCreator;
 
+  let charData = {
+    class: 'Mage',
+    gender: 'Female',
+    funFact: 'Always Eternal',
+  };
+
   beforeEach(() => {
     characterCreator = new CharacterCreator();
   });
 
-  test("should process data correctly when written to", (done) => {
-    // TODO: Write your test here
+  // Test 1: The CharacterCreator processes data correctly when written to.
+  test('should process data correctly when written to', (done) => {
+    characterCreator.write(JSON.stringify(charData));
+    characterCreator.on('data', (data) => {
+      const expectedOutput =
+      `The character is a ${charData.gender} class ${charData.class} who is ${charData.funFact}`;
+      expect(data.toString().trim()).toBe(expectedOutput);
+      done();
+    });
   });
 
+  // Test 2: The CharacterCreator emits an “error” event when an empty string is written to it.
   test("should emit 'error' when invalid data is written", (done) => {
-    // TODO: Write your test here
+    characterCreator.on('error', (err) => {
+      expect(err.message).toBe('Invalid character data');
+      done();
+    });
+    characterCreator.write('');
   });
 
-  test("should transform data correctly when written to", (done) => {
-    // TODO: Write your test here
+  // Test 3: The CharacterCreator transforms the data correctly when written to.
+  test('should transform data correctly when written to', (done) => {
+    const expectedOutput =
+      `The character is a ${charData.gender} class ${charData.class} who is ${charData.funFact}`;
+    characterCreator.write(JSON.stringify(charData), 'utf8', () => {
+      characterCreator.on('data', (data) => {
+        expect(data.toString().trim()).toBe(expectedOutput);
+        done();
+      });
+    });
   });
 });
